6abb084cfda7ab59277b6f01ad0cba3ed01677f2
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-23 16:33:20 +0100

    Create Row styled component and apply

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import GlobalSyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: red;
   padding: 20px;
 `;
 
@@ -14,12 +14,23 @@ function App() {
     <>
       <GlobalSyles />
       <StyledApp>
-        <Heading as="h1">Inn Sight</Heading>
-        <Heading as="h2">Check In and Out</Heading>
-        <Button onClick={() => alert("check in")}>Check In</Button>
-        <Button onClick={() => alert("check out")}>Check Out</Button>
-        <Heading as="h3">Form</Heading>
-        <Input type="number" placeholder="Number of guess" />
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Inn Sight</Heading>
+            <div>
+              <Heading as="h2">Check In and Out</Heading>
+              <Button onClick={() => alert("check in")}>Check In</Button>
+              <Button onClick={() => alert("check out")}>Check Out</Button>
+            </div>
+          </Row>
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guess" />
+              <Input type="number" placeholder="Number of guess" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
