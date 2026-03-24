e42fce65823bdf80181f00ac6151414c10480c37
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-24 14:39:39 +0100

    Add styled component to header, aside and main html tags

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,13 @@
+import styled from "styled-components";
+
 function Header() {
-  return <header>Header</header>;
+  const StyledHeader = styled.header`
+    background-color: orange;
+    padding: 1.2rem 4.8rem;
+    border-bottom: 1px solid var(--color-grey-100);
+  `;
+
+  return <StyledHeader>Header</StyledHeader>;
 }
 
 export default Header;
