8348710309ad5c0060e6a72d59c3d6faae1a239f
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-03-24 14:51:00 +0100

    Add styles to sidebar, header and applayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,19 +4,26 @@ import Header from "./Header";
 import styled from "styled-components";
 
 const Main = styled.main`
-  background-color: green;
+  background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+`;
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100vh;
 `;
 
 function AppLayout() {
   return (
-    <div>
+    <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
         <Outlet />
       </Main>
-    </div>
+    </StyledAppLayout>
   );
 }
 
