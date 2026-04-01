0fdfab314bafb5c927ad521a333b52b4ac05d185
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-01 11:40:24 +0200

    Create Styled Modal component and apply

--- src/features/cabins/AddCabins.jsx
+++ src/features/cabins/AddCabins.jsx
@@ -2,16 +2,21 @@ import { useState } from "react";
 
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
+import Modal from "../../ui/Modal";
 
 function AddCabins() {
-  const [showForm, setShowForm] = useState(false);
+  const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
     <div>
-      <Button onClick={() => setShowForm((show) => !show)}>
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
         Add new Cabin
       </Button>
-      {showForm && <CreateCabinForm />}
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
+        </Modal>
+      )}
     </div>
   );
 }
--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,7 @@ import Textarea from "../../ui/Textarea";
 import { useCreateCabin } from "./useCreateCabin";
 import { useEditCabin } from "./useEditCabin";
 
-function CreateCabinForm({ cabinToEdit = {} }) {
+function CreateCabinForm({ cabinToEdit = {}, onCloseModal }) {
   const { id: editId, ...editValues } = cabinToEdit;
   const isEditSession = Boolean(editId);
 
@@ -28,10 +28,23 @@ function CreateCabinForm({ cabinToEdit = {} }) {
     if (isEditSession)
       editCabin(
         { newCabinData: { ...data, image }, id: editId },
-        { onSuccess: (data) => reset() },
+        {
+          onSuccess: (data) => {
+            reset();
+            onCloseModal?.();
+          },
+        },
       );
     else
-      createCabin({ ...data, image: image }, { onSuccess: (data) => reset() }); //adds image to the data object
+      createCabin(
+        { ...data, image: image },
+        {
+          onSuccess: (data) => {
+            reset();
+            onCloseModal?.();
+          },
+        },
+      ); //adds image to the data object
   }
 
   function onError(errors) {
@@ -113,7 +126,11 @@ function CreateCabinForm({ cabinToEdit = {} }) {
 
       <FormRow>
         {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
+        <Button
+          variation="secondary"
+          type="reset"
+          onClick={() => onCloseModal?.()}
+        >
           Cancel
         </Button>
         <Button disable={isWorking}>
