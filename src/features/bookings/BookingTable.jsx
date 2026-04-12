a3564e8335a6bde25d4f14a8c47f550cdc1288c5
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-12 10:58:37 +0200

    Create Bookings page, fetch data from superbase and display table

---
+++ src/features/bookings/BookingTable.jsx
@@ -0,0 +1,43 @@
+// import BookingRow from "./BookingRow";
+import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
+import Empty from "../../ui/Empty";
+import Spinner from "../../ui/Spinner";
+import BookingRow from "./BookingRow";
+import { useBookings } from "./useBookings";
+
+function BookingTable() {
+  const { bookings, isLoading } = useBookings();
+
+  console.log(bookings);
+
+  if (isLoading) return <Spinner />;
+
+  if (!bookings.length) return <Empty resourceName="bookings" />;
+
+  console.log(bookings);
+
+  return (
+    <Menus>
+      <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+        <Table.Header>
+          <div>Cabin</div>
+          <div>Guest</div>
+          <div>Dates</div>
+          <div>Status</div>
+          <div>Amount</div>
+          <div></div>
+        </Table.Header>
+
+        <Table.Body
+          data={bookings}
+          render={(booking) => (
+            <BookingRow key={booking.id} booking={booking} />
+          )}
+        />
+      </Table>
+    </Menus>
+  );
+}
+
+export default BookingTable;
