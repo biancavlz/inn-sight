e4709d762309e098b8e703f1978482c4d6c4dad0
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-15 12:25:19 +0200

    Add Authorization functionality with protectedRoute and requesting user data

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,15 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isPending: isLogingIn } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      console.log(user);
+      queryClient.setQueriesData(["user"], user); //stores user in react-query cache, avoid to re-fetch the user data
       navigate("/dashboard");
     },
     onError: (err) => {
