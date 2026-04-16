5b2ae309346897e2ee777832e8b80c80e297d1b5
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-16 14:39:14 +0200

    Add User account update data and avatar

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -43,3 +43,40 @@ export async function logout() {
 
   if (error) throw new Error(error.message);
 }
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password or fullName
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName)
+    updateData = {
+      data: {
+        fullName,
+      },
+    };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+
+  if (!avatar) return data;
+
+  // 2. Upload avatar image
+  const filename = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(filename, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // 3. Update user with avatar
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${import.meta.env.VITE_SUPABASE_URL}/storage/v1/object/public/avatars/${filename}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+
+  return updatedUser;
+}
