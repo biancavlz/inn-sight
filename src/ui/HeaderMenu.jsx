9c7eaf102dc64ae8c5dfaebf9ea4672f6b266aa9
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-16 11:18:41 +0200

    Create HeaderMenu.jsx

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+`;
+
+function HeaderMenu() {
+  return <StyledHeaderMenu />;
+}
+
+export default HeaderMenu;
