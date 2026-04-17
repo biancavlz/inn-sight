4989f1f83faa618c7a6ae6cb89727a10b66d7e55
Bianca Velázquez <biancavlz@users.noreply.github.com>
2026-04-17 09:47:25 +0200

    Filter bookings to getBookingsAfterDate

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,18 @@
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import DashboardFilter from "../features/dashboard/DashboardFilter";
 import Heading from "../ui/Heading";
+import Spinner from "../ui/Spinner";
 import Row from "../ui/Row";
 
+import { useRecentBookings } from "../features/dashboard/useRecentBookings";
+
 function Dashboard() {
+  const { isLoading, bookings } = useRecentBookings();
+
+  if (isLoading) return <Spinner />;
+
+  console.log(bookings);
+
   return (
     <>
       <Row type="horizontal">
